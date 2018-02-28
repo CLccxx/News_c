e251d3912a89c04ba6bc29a8b9dd0bfa83337a5a
Clccxx <245570636@qq.com>
2018-02-28 15:54:51 +0800

    项目搭建+添加素材

---
+++ News_c/News_cTests/News_cTests.swift
@@ -0,0 +1,36 @@
+//
+//  News_cTests.swift
+//  News_cTests
+//
+//  Created by 杨博兴 on 2018/2/28.
+//  Copyright © 2018年 xx_cc. All rights reserved.
+//
+
+import XCTest
+@testable import News_c
+
+class News_cTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
