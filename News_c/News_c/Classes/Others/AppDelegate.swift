a0294961b5660e623cff52d26ddbdcb1f870ae82
Clccxx <245570636@qq.com>
2018-03-12 17:58:39 +0800

    修改copyright

--- News_c/News_c/Classes/Others/AppDelegate.swift
+++ News_c/News_c/Classes/Others/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  News
 //
-//  Created by 杨蒙 on 2017/9/6.
-//  Copyright © 2017年 hrscy. All rights reserved.
+//  Created by 杨博兴 on 2018/2/28.
+//  Copyright © 2018年 xx_cc. All rights reserved.
 //
 
 import UIKit
